13bee8588c08db7e3d2e77fb2184192afdd68213
ohotuowo-morgan <125ohotumorgan@gmail.com>
2026-06-10 11:05:54 +0100

    feat: env cleanup

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,3 @@
-
-// import { defineConfig } from 'vite';
-// import tailwindcss from '@tailwindcss/vite';
-// import { resolve } from 'path';
-
-// export default defineConfig({
-//   root: 'src', 
-//   plugins: [
-//     tailwindcss(),
-//   ],
-//   server: {
-//     port: 3000,
-//     open: true
-//   },
-//   build: {
-//     outDir: '../dist', 
-//     emptyOutDir: true,
-//     rollupOptions: {
-//       input: {
-//         main: resolve(__dirname, 'src/index.html'),
-//         matchIngredients: resolve(__dirname, 'src/match-ingredients/index.html'),
-//         browseRecipes: resolve(__dirname, 'src/browse-recipes/index.html'),
-//         recipeBox: resolve(__dirname, 'src/recipe-box/index.html')
-//       }
-//     }
-//   }
-// });
-
 import { defineConfig } from 'vite';
 import tailwindcss from '@tailwindcss/vite';
 import { resolve } from 'path';
